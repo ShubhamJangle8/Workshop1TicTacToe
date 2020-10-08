5f133b0903403a4ac414e1a5d8420c3992fa8a68
jangle <shubhamjangale0000@gmail.com>
2020-10-08 09:22:23 +0530

    Started : By creating a array for TicTacToe game

---
+++ src/com/game/TicTacToeGame.java
@@ -0,0 +1,19 @@
+package com.game;
+
+public class TicTacToeGame {
+	public char board[][] ;
+	public void createBoard(){
+		board = new char[4][4];
+		for(int i = 1;i <= 3 ; i++) {
+			for(int j = 1 ; j <= 3 ; j++) {
+				board[i][j] = ' ';
+				System.out.println(board[i][j]);
+			}
+		}
+	}
+	public static void main(String[] args) {
+		TicTacToeGame gameBoard = new TicTacToeGame();
+		gameBoard.createBoard();
+		
+	}
+}
