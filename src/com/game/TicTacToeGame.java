db5eebc1ba84b939f228562ed0f8e518649c99b9
jangle <shubhamjangale0000@gmail.com>
2020-10-08 10:01:16 +0530

    Refactor : Adding functionality to get the input for the player

--- src/com/game/TicTacToeGame.java
+++ src/com/game/TicTacToeGame.java
@@ -1,7 +1,26 @@
 package com.game;
+import java.util.Scanner;
 
 public class TicTacToeGame {
+	public char player;
+	public char computer;
 	public char board[][] ;
+	public void chooseSymbol() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Player please enter the symbol : ");
+		player = input.next().charAt(0);
+		if(player == 'X') {
+			computer = 'O';
+			System.out.println("Computer gets " + computer);
+		}
+		else if(player == 'O'){
+			computer = 'X';
+			System.out.println("Computer gets " + computer);
+		}
+		else {
+			System.out.println("Please enter a valid input !");
+		}
+	}
 	public void createBoard(){
 		board = new char[4][4];
 		for(int i = 1 ; i <= 3 ; i++) {
@@ -13,5 +32,7 @@ public class TicTacToeGame {
 	public static void main(String[] args) {
 		TicTacToeGame gameBoard = new TicTacToeGame();
 		gameBoard.createBoard();
+		gameBoard.chooseSymbol();
+		
 	}
 }
