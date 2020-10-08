cb384fff00e134a4f2bdcbd4193e69e3414f9c2d
jangle <shubhamjangale0000@gmail.com>
2020-10-08 09:57:12 +0530

    Started : To create a Board

--- src/com/game/TicTacToeGame.java
+++ src/com/game/TicTacToeGame.java
@@ -4,16 +4,14 @@ public class TicTacToeGame {
 	public char board[][] ;
 	public void createBoard(){
 		board = new char[4][4];
-		for(int i = 1;i <= 3 ; i++) {
+		for(int i = 1 ; i <= 3 ; i++) {
 			for(int j = 1 ; j <= 3 ; j++) {
 				board[i][j] = ' ';
-				System.out.println(board[i][j]);
 			}
 		}
 	}
 	public static void main(String[] args) {
 		TicTacToeGame gameBoard = new TicTacToeGame();
 		gameBoard.createBoard();
-		
 	}
 }
